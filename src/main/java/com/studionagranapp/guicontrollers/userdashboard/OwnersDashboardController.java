c15f9a028adbf14db8367199c2a8f061d2e4f7d2
Mateusz Muzioł <matias3004@gmail.com>
2022-12-14 11:03:06 +0100

    Basic UI for the owner

--- src/main/java/com/studionagranapp/guicontrollers/userdashboard/OwnersDashboardController.java
+++ src/main/java/com/studionagranapp/guicontrollers/userdashboard/OwnersDashboardController.java
@@ -1,4 +1,12 @@
 package com.studionagranapp.guicontrollers.userdashboard;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class OwnersDashboardController {
+
+    @FXML
+    private void logout() {
+
+    }
 }
