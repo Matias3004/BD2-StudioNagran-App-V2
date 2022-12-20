45d3a1ce66afa7cf27dbf9a149f74ce62d74ea2a
Mateusz Muzioł <matias3004@gmail.com>
2022-12-20 17:32:29 +0100

    Client's dashboard now shows available backline

--- src/main/java/com/studionagranapp/guicontrollers/userdashboard/ClientsDashboardController.java
+++ src/main/java/com/studionagranapp/guicontrollers/userdashboard/ClientsDashboardController.java
@@ -1,14 +1,28 @@
 package com.studionagranapp.guicontrollers.userdashboard;
 
+import com.studionagranapp.helpers.configurators.tableconfigurators.EquipmentTableConfigurator;
+import com.studionagranapp.helpers.databaseconnection.DatabaseManager;
 import com.studionagranapp.helpers.errorhandling.AlertManager;
 import com.studionagranapp.helpers.contentloaders.SceneCreator;
+import com.studionagranapp.helpers.models.Equipment;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class ClientsDashboardController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientsDashboardController implements Initializable {
 
     @FXML
     private Button logoutButton;
@@ -16,14 +30,32 @@ public class ClientsDashboardController {
     private BorderPane borderPane;
     @FXML
     private Label userInfo;
+    @FXML
+    TableView<Equipment> equipmentTable;
+    @FXML
+    TableColumn<Equipment, String> eqNameColumn;
+    @FXML
+    TableColumn<Equipment, Integer> eqTypeColumn;
+
+    private DatabaseManager databaseManager;
+    private EquipmentTableConfigurator equipmentTableConfigurator;
+    private final ObservableList<Equipment> equipmentObservableList = FXCollections.observableArrayList();
     private final AlertManager alertManager;
 
     public ClientsDashboardController() {
+        databaseManager = DatabaseManager.getInstance();
+        equipmentTableConfigurator = new EquipmentTableConfigurator();
         alertManager = new AlertManager();
     }
 
     public void setUserInfo(String userInfo) {
         this.userInfo.setText(userInfo);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        equipmentTableConfigurator.provideClientConfiguration(equipmentObservableList, equipmentTable);
+        initData();
     }
 
     @FXML
@@ -36,5 +68,25 @@ public class ClientsDashboardController {
     @FXML
     public void bookSession() {
 
+    }
+
+    @FXML
+    private void refresh() {
+        equipmentTableConfigurator.provideClientConfiguration(equipmentObservableList, equipmentTable);
+        initData();
+    }
+
+    private void initData() {
+        eqNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        eqTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+
+        equipmentTable.setItems(equipmentObservableList);
+
+        FilteredList<Equipment> filteredData = new FilteredList<>(equipmentObservableList, b -> true);
+
+        SortedList<Equipment> sortedData = new SortedList<>(filteredData);
+        sortedData.comparatorProperty().bind(equipmentTable.comparatorProperty());
+
+        equipmentTable.setItems(sortedData);
     }
 }
